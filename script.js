46d466e7340781db742bdc028e3da89b5674a680
Dani Roman <droman@hubos.com>
2024-06-13 16:43:54 +0200

    DRM: Initial exercice

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function invertirTexto() {
-    const textoOriginal = document.getElementById('texto-original').value;
-    const textoInvertido = textoOriginal.split('').reverse().join('');
-    document.getElementById('texto-invertido').textContent = textoInvertido;
-  }
-  
+document.getElementById('submitButton').addEventListener('click', function() {
+    // Obtener el valor del campo de texto
+    var inputText = document.getElementById('inputText').value;
+
+    // Invertir la cadena de texto
+    var reversedText = inputText.split('').reverse().join('');
+
+    // Mostrar el resultado
+    document.getElementById('resultText').innerText = reversedText;
+});
